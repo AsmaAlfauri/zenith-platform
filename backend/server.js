617f5fc6a61a975d41d49054188422f9718643a6
AsmaaAlfauri <asma.meraki23@gmail.com>
2026-01-24 00:28:49 +0400

    CORS config

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-// app.use(cors());
-app.use(cors({
-  origin: 'https://zenith-platform.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
